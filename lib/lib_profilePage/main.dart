ffe2654b67eb8b3fb75accda7362260a1d6e7f6f
jordhi404 <79629470+jordhi404@users.noreply.github.com>
2022-04-13 04:39:56 +0700

    Update main.dart

--- lib/lib_profilePage/main.dart
+++ lib/lib_profilePage/main.dart
@@ -15,6 +15,25 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final List<String> entries = <String>['Nama User', 'Alamat', 'Kontak', 'Email'];
+    final List<int> colorCodes = <int>[600, 500, 300, 100];
+
+    ListView.separated(
+      padding: const EdgeInsets.all(8),
+      itemCount: entries.length,
+      itemBuilder: (BuildContext context, int index){
+        return Container(
+          height: 50,
+          color: Colors.amber,
+          child: Center(
+            child: Text(
+              '${entries[index]}'
+            )
+          ),
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) => const Divider(),
+    );
     return Scaffold(
       backgroundColor: Color.fromARGB(210, 201, 201, 87),
       body: SafeArea(
